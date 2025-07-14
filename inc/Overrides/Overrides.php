13a1ed9f63c65e9b779bef7c001fff7a1dca01d0
Paul Kevan <pk@automattic.com>
2025-07-14 14:45:49 +0100

    readability--

--- inc/Overrides/Overrides.php
+++ inc/Overrides/Overrides.php
@@ -89,18 +89,18 @@ final class Overrides {
 
 	public function wp_post_to_insert_array( WP_Post $post ): array {
 		return [
-			'post_title'        => $post->post_title,
-			'post_content'      => $post->post_content,
-			'post_excerpt'      => $post->post_excerpt,
-			'post_status'       => $post->post_status,
-			'post_type'         => $post->post_type,
-			'post_author'       => $post->post_author,
-			'post_date'         => $post->post_date,
-			'post_date_gmt'     => $post->post_date_gmt,
-			'post_modified'     => $post->post_modified,
+			'post_title' => $post->post_title,
+			'post_content' => $post->post_content,
+			'post_excerpt' => $post->post_excerpt,
+			'post_status' => $post->post_status,
+			'post_type' => $post->post_type,
+			'post_author' => $post->post_author,
+			'post_date' => $post->post_date,
+			'post_date_gmt' => $post->post_date_gmt,
+			'post_modified' => $post->post_modified,
 			'post_modified_gmt' => $post->post_modified_gmt,
-			'post_name'         => $post->post_name,
-			'post_parent'       => $post->post_parent,
+			'post_name' => $post->post_name,
+			'post_parent' => $post->post_parent,
 		];
 	}
 }
