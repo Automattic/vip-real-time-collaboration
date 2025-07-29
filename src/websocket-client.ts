0d83a294597b4a9163e6c5e4ba03630c337c2d16
chriszarate <chris.zarate@automattic.com>
2025-07-29 16:44:00 -0400

    Implement sync provider with WebSocket connection

---
+++ src/websocket-client.ts
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import { WebsocketProvider } from 'y-websocket';
+
+import type { ConnectDoc } from '@wordpress/sync';
+import type * as Y from 'yjs';
+
+type WebsocketProviderConstructorArgs = ConstructorParameters< typeof WebsocketProvider >;
+
+interface WebsocketConnectionConfig {
+	options?: WebsocketProviderConstructorArgs[ 3 ];
+	password?: string; // TODO: Use this to authorize the connection
+	serverUrl: string;
+}
+
+/**
+ * Function that creates a new WebSocket Connection.
+ *
+ * @param {WebsocketConnectionConfig} config The configuration for the WebSocket connection.
+ * @return {ConnectDoc} A function that connects a Y.Doc to a WebSocket server.
+ */
+export function createWebSocketConnection( config: WebsocketConnectionConfig ): ConnectDoc {
+	return function ( objectId: string, objectType: string, doc: Y.Doc ) {
+		const roomName = `${ objectType }-${ objectId }`;
+		let provider = null;
+
+		try {
+			provider = new WebsocketProvider( config.serverUrl, roomName, doc, config.options );
+		} catch {}
+
+		return Promise.resolve( {
+			awareness: provider?.awareness || null,
+			destroy: () => {
+				// The WebsocketProvider handles its own cleanup. If needed, we could
+				// implement a way to disconnect or clean up resources here.
+			},
+		} );
+	};
+}
--- types/wordpress__sync/index.d.ts
+++ types/wordpress__sync/index.d.ts
@@ -1,5 +1,63 @@
-type ConnectDoc = ( id: ObjectID, type: ObjectType, ydoc: Y.Doc ) => Promise< () => void >;
+/**
+ * External dependencies
+ */
+import type { Awareness } from 'y-protocols/awareness';
+import type * as Y from 'yjs';
 
-interface RemoteConnectionCreators {
-	createWebSocketConnection: ( options: { serverUrl: string } ) => ConnectDoc;
+declare module '@wordpress/sync' {
+	type ObjectID = string;
+	type ObjectType = string;
+	type ObjectData = object;
+	type UndoManager = Y.UndoManager;
+
+	type AwarenessClientID = number;
+
+	type AwarenessEventListener = ( params: {
+		added: AwarenessClientID[];
+		updated: AwarenessClientID[];
+		removed: AwarenessClientID[];
+	} ) => void;
+
+	type AwarenessStates = Map< AwarenessClientID, Record< string, unknown > >;
+
+	type CRDTDoc = Y.Doc;
+
+	type ConnectDocResult = {
+		awareness: Awareness | null;
+		destroy: () => void;
+	};
+
+	type ConnectDoc = ( id: ObjectID, type: ObjectType, ydoc: Y.Doc ) => Promise< ConnectDocResult >;
+
+	type SyncConfig = {
+		applyChangesToDoc: ( ydoc: Y.Doc, data: Partial< ObjectData > ) => void;
+		fromCRDTDoc: ( ydoc: Y.Doc ) => ObjectData;
+		getObjectId: ( data: ObjectData ) => ObjectID;
+		objectType: ObjectType;
+		supportsAwareness: boolean;
+	};
+
+	type SyncProvider = {
+		__fallback?: boolean;
+		bootstrap: (
+			syncConfig: SyncConfig,
+			initialData: ObjectData,
+			handleChanges: ( data: Partial< ObjectData > ) => void
+		) => Promise< void >;
+		configs: Map< ObjectType, SyncConfig >;
+		discard: ( type: ObjectType, id: ObjectID ) => void;
+		update: (
+			type: ObjectType,
+			record: ObjectData,
+			changes: Partial< ObjectData >,
+			origin: string
+		) => void;
+
+		awarenessManager?: {
+			addListener: ( eventType: 'update' | 'change', listener: AwarenessEventListener ) => void;
+			getStates: () => AwarenessStates;
+			setLocalState: ( field: string, value: unknown ) => void;
+			removeStates: () => void;
+		};
+	};
 }
