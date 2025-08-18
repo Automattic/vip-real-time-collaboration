53fad80cc1de8b83bd8adf58ca670490334629cb
ingeniumed <gopal.krishnan@automattic.com>
2025-08-18 15:13:34 +1000

    Add some more comments

--- tests/e2e/specs/sync-changes.spec.ts
+++ tests/e2e/specs/sync-changes.spec.ts
@@ -54,8 +54,12 @@ test.describe( 'The plugin should sync changes between multiple browser sesssion
 		// Wait for it to finish loading
 		await existingPostPage.waitForLoadState();
 
+		// ToDo: Sometimes, the block editor doesn't get auto-focused so the tests fails. Re-running fixes it, but that's not right.
+
+		// Go to the end of the existing paragraph
 		await existingPostPage.keyboard.press( 'End' );
 
+		// Modify it from the new context
 		await existingPostPage.keyboard.type(
 			' This was modified from another another browser context.'
 		);
@@ -63,6 +67,7 @@ test.describe( 'The plugin should sync changes between multiple browser sesssion
 		// Get the title and content of the edited post, from the old context
 		const pageHTML = await editor.getEditedPostContent();
 
+		// Ensure the contexts are different
 		expect( oldContext === newContext ).toBeFalsy();
 
 		// Ensure the pageHTML for the original post containts the new changes we put in place
@@ -70,6 +75,7 @@ test.describe( 'The plugin should sync changes between multiple browser sesssion
 			'<!-- wp:paragraph -->\n<p>This is a paragraph. This was modified from another another browser context.</p>\n<!-- /wp:paragraph -->'
 		);
 
+		// Close the new context
 		await existingPostPage.close();
 		await newContext.close();
 	} );
