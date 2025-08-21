19fb08badbbc0ba721f8618ef3a16220046f8fa1
chriszarate <chris.zarate@automattic.com>
2025-08-20 18:49:14 -0600

    __

--- src/components/post-locked-modal.tsx
+++ src/components/post-locked-modal.tsx
@@ -33,8 +33,14 @@ export function PostLockedModal() {
 					<Icon fill="#ccc" icon={ error } size={ 64 } />
 					<h1>{ __( 'Disconnected', 'vip-real-time-collaboration' ) }</h1>
 					<p className="vip-rtc-post-locked-modal__p">
-						You are currently disconnected from the collaborative editing server.{ ' ' }
-						<strong>Editing is temporarily disabled</strong> to prevent conflicts with other users.
+						{ __(
+							'You are currently disconnected from the collaborative editing server.',
+							'vip-real-time-collaboration'
+						) }{ ' ' }
+						{ __(
+							'Editing is temporarily disabled to prevent conflicts with other users.',
+							'vip-real-time-collaboration'
+						) }
 					</p>
 					<HStack spacing={ 2 } justify="center">
 						<Button href="edit.php" isDestructive={ true } variant="secondary">
