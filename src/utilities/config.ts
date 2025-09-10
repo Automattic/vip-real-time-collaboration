3cde81061c4dc517a870d41e7c8526aa8e2a156a
Shekhar Wagh <shekhar.wagh@automattic.com>
2025-09-10 22:16:09 +0530

    throw when window.VIP_is not available

--- src/utilities/config.ts
+++ src/utilities/config.ts
@@ -1,4 +1,4 @@
-export const PERSISTED_STATE_POST_META_KEY = getVipConfigFromWindow( 'rtcPostMetaKey' ) ?? '';
+export const PERSISTED_STATE_POST_META_KEY = getVipConfigFromWindow( 'rtcPostMetaKey' );
 
 export const AWARENESS_CURSOR_UPDATE_DEBOUNCE_IN_MS = 150;
 export const LOCAL_CURSOR_UPDATE_DEBOUNCE_IN_MS = 20;
@@ -7,7 +7,7 @@ export const DISCONNECTED_THRESHOLD_IN_MS = 5000;
 export const REMOVAL_DELAY_IN_MS = 5000;
 
 export const WEBSOCKET_PROVIDER_MAX_BACKOFF_IN_MS = 15000;
-export const WEBSOCKET_URL = getVipConfigFromWindow( 'wsUrl' ) ?? '';
+export const WEBSOCKET_URL = getVipConfigFromWindow( 'wsUrl' );
 
 export const BLOG_ID = getVipConfigFromWindow( 'blogId' );
 
@@ -16,11 +16,9 @@ export function isDevelopment(): boolean {
 	return 'development' === process.env.NODE_ENV;
 }
 
-function getVipConfigFromWindow< Key extends keyof VIPRTCConfig >(
-	key: Key
-): VIPRTCConfig[ Key ] | null {
+function getVipConfigFromWindow< Key extends keyof VIPRTCConfig >( key: Key ): VIPRTCConfig[ Key ] {
 	if ( 'undefined' === typeof window || 'undefined' === typeof window.VIP_RTC ) {
-		return null;
+		throw new Error( 'window.VIP_RTC is not defined' );
 	}
 
 	return window.VIP_RTC[ key ];
