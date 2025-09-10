e9afc494d8d402da69a0c34428eda134a57bf447
Alec Geatches <alec.geatches@automattic.com>
2025-09-10 12:13:43 -0600

    Ensure iframe document is ready before appending to body

---
+++ src/hooks/use-document-ready.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from '@wordpress/element';
+
+/**
+ * Hook that tracks whether a document is ready. We use this instead
+ * of `domReady()` from WordPress because it allows us to specify
+ * which document to check, useful for iframe documents.
+ *
+ * @param document - The document to check for readiness
+ * @returns boolean indicating if the document is ready
+ */
+export function useDocumentReady( document?: Document | null ): boolean {
+	const [ isReady, setIsReady ] = useState( false );
+
+	useEffect( () => {
+		if ( ! document ) {
+			setIsReady( false );
+			return;
+		}
+
+		const ready = ( readyCallback: () => void ) => {
+			if ( document.readyState !== 'loading' ) {
+				readyCallback();
+				return;
+			}
+
+			document.addEventListener( 'DOMContentLoaded', readyCallback );
+		};
+
+		ready( () => {
+			setIsReady( true );
+		} );
+	}, [ document ] );
+
+	return isReady;
+}
