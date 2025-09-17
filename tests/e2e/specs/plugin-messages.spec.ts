bb8d6ac47afd57c58ae72df5ad9c1cd6d955c9ae
ingeniumed <ingeniumed@users.noreply.github.com>
2025-09-17 15:06:19 +1000

    Fix the integration tests, there's 3 still failing

--- tests/e2e/specs/plugin-messages.spec.ts
+++ tests/e2e/specs/plugin-messages.spec.ts
@@ -7,6 +7,11 @@ import { expect, test } from '@wordpress/e2e-test-utils-playwright';
  * Tests the messages displayed by the plugin.
  */
 test.describe( 'The plugin should display', () => {
+	test.beforeAll( async ( { requestUtils } ) => {
+		await requestUtils.activatePlugin( 'gutenberg' );
+		await requestUtils.activatePlugin( 'vip-real-time-collaboration' );
+	} );
+
 	test.afterEach( async ( { requestUtils } ) => {
 		await requestUtils.deleteAllPosts();
 	} );
@@ -14,17 +19,19 @@ test.describe( 'The plugin should display', () => {
 	/**
 	 * Verifies that the plugin displays error when Gutenberg is not active.
 	 */
-	test( 'An error when Gutenberg is not active', async ( { admin } ) => {
+	test( 'An error when Gutenberg is not active', async ( { admin, requestUtils } ) => {
 		const page = admin.page;
 		const errorMessage =
 			'The Gutenberg plugin has not been installed. The VIP Real-Time Collaboration plugin has been disabled.';
 
 		await admin.visitAdminPage( '/plugins.php' );
 
-		await page.getByRole( 'link', { name: 'Deactivate Gutenberg' } ).click();
+		await requestUtils.deactivatePlugin( 'gutenberg' );
+		await page.reload();
 		await expect( page.getByText( errorMessage ) ).toBeVisible();
 
-		await page.getByRole( 'link', { name: 'Activate Gutenberg' } ).click();
+		await requestUtils.activatePlugin( 'gutenberg' );
+		await page.reload();
 		await expect( page.getByText( errorMessage ) ).not.toBeVisible();
 	} );
 } );
