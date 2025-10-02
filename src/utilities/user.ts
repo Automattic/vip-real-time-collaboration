39195d88a740a40c16dcb5e388f4b8a8e6864573
Alec Geatches <alec.geatches@automattic.com>
2025-10-02 11:58:12 -0600

    Simplify areUserStatesEqual to just use JSON.

--- src/utilities/user.ts
+++ src/utilities/user.ts
@@ -1,23 +1,5 @@
-import { type EditorState, type UserState } from '@/store/awareness-store';
-import { areSelectionsEqual } from '@/utilities/selection';
-
-function areEditorStatesEqual( state1?: EditorState, state2?: EditorState ): boolean {
-	return Boolean( state1 && state2 && areSelectionsEqual( state1.selection, state2.selection ) );
-}
+import { type UserState } from '@/store/awareness-store';
 
 export function areUserStatesEqual( userState1: UserState, userState2: UserState ): boolean {
-	if ( Object.keys( userState1 ).length !== Object.keys( userState2 ).length ) {
-		return false;
-	}
-
-	return Object.entries( userState1 ).every( ( [ key, value ] ) => {
-		// Update this switch with any non-primitive fields added to UserState.
-		switch ( key ) {
-			case 'editorState':
-				return areEditorStatesEqual( userState1.editorState, userState2.editorState );
-
-			default:
-				return value === userState2[ key as keyof UserState ];
-		}
-	} );
+	return JSON.stringify( userState1 ) === JSON.stringify( userState2 );
 }
