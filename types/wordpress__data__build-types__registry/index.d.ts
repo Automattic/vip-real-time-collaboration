8875d3828dfe3ec63ff4f18daccd222d80ba864a
ingeniumed <ingeniumed@users.noreply.github.com>
2025-10-24 08:10:25 +1100

    Remove the extra types

--- types/wordpress__data__build-types__registry/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@wordpress/data/build-types/registry' {
-	export interface WPDataRegistry {
-		dispatch( namespaceName: string ): Record< string, ( ...args: unknown[] ) => void >;
-	}
-}
--- types/wordpress__editor__build-types__store__selectors/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@wordpress/editor/build-types/store/selectors' {
-	export interface WPBlockSelection {
-		clientId: string;
-		attributeKey: string;
-		offset: number;
-	}
-}
