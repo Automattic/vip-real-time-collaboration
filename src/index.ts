25d516a7fbbe5701a3e8096a94e04967732511b0
ingeniumed <ingeniumed@users.noreply.github.com>
2025-10-24 09:30:41 +1100

    Correct the lint failure

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { addFilter } from '@wordpress/hooks';
 import { registerPlugin } from '@wordpress/plugins';
 
+import { ReadOnlyCodeEditor } from '@/components/read-only-code-editor';
 import { RTCSettingsPanel } from '@/components/rtc-settings-panel';
 import { WEBSOCKET_URL } from '@/utilities/config';
 import { Logger } from '@/utilities/logger';
 import { createWebSocketConnection } from '@/websocket-client';
-import { ReadOnlyCodeEditor } from '@/components/read-only-code-editor';
 
 addFilter( 'sync.providers', 'vip-rtc', () => {
 	// We already error check for the WebSocket URL in the main plugin file,
