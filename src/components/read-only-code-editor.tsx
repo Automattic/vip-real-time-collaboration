9c2af4a7500f44460ef9a8f75a721798872ae32a
Max Schmeling <max.schmeling@automattic.com>
2025-10-23 15:48:03 -0600

    Add isTextAreaElement type guard

--- src/components/read-only-code-editor.tsx
+++ src/components/read-only-code-editor.tsx
@@ -1,10 +1,14 @@
 /**
  * WordPress dependencies
  */
-import { useEffect } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { store as editPostStore, type EditPostStoreSelectors } from '@wordpress/edit-post';
+import { useEffect } from '@wordpress/element';
 import { applyFilters } from '@wordpress/hooks';
+
+function isTextAreaElement( element: Element | null ): element is HTMLTextAreaElement {
+	return element?.matches( 'textarea' ) ?? false;
+}
 
 export function ReadOnlyCodeEditor() {
 	// Get the current editor mode (visual or text).
@@ -18,13 +22,14 @@ export function ReadOnlyCodeEditor() {
 	// When in text mode, set the code editor (textarea) to read-only.
 	useEffect( () => {
 		if ( editorMode === 'text' ) {
-			const textarea = document.querySelector< HTMLTextAreaElement >( '.editor-post-text-editor' );
-			if ( textarea ) {
+			const editorPostTextEditorElement = document.querySelector( '.editor-post-text-editor' );
+
+			if ( isTextAreaElement( editorPostTextEditorElement ) ) {
 				// Add a filter here to customize this value, based on any external logic.
 				const readOnly = applyFilters( 'vip_rtc_read_only_code_editor', true );
 
 				// Ensure we have a boolean value only, defaulting to true for safety.
-				textarea.readOnly = typeof readOnly === 'boolean' ? readOnly : true;
+				editorPostTextEditorElement.readOnly = typeof readOnly === 'boolean' ? readOnly : true;
 			}
 		}
 	}, [ editorMode ] );
