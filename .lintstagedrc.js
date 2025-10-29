be61b30925099b45b398bf8a344c02850bbc1d29
ingeniumed <ingeniumed@users.noreply.github.com>
2025-10-30 10:29:46 +1100

    ADd a missing comment for linstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,11 @@
 module.exports = {
 	'*.css': [ 'npm run lint:css' ],
+	// This is needed because passing in files to tsc, means that
+	// the tsconfig.json is ignored and thus project settings are not applied.
+	// Lint-staged always passes in the files to the commands, so we have to force it
+	// to skip sending those files by using a function here.
+	// https://github.com/lint-staged/lint-staged/issues/825
+	// https://github.com/microsoft/TypeScript/issues/6591
 	'*.{js,jsx,ts,tsx}': [ () => 'npm run lint' ],
 	'*.php': [ 'npm run lint:php' ],
 	'*.{js,json,jsx,md,ts,tsx,yml,yaml}': [ 'npm run format:check' ],
