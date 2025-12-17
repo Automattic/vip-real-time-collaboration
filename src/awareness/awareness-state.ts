ed3e3848b97eccd65ac8f700b43f45a286f71adb
Alec Geatches <alec.geatches@automattic.com>
2025-12-17 13:28:45 -0700

    Remove double filter
    Co-authored-by: Chris Zarate <chris.zarate@automattic.com>

--- src/awareness/awareness-state.ts
+++ src/awareness/awareness-state.ts
@@ -253,7 +253,6 @@ export abstract class AwarenessState<
 
 		const updatedStates = new Map< number, EnhancedState< State > >(
 			[ ...this.disconnectedUsers, ...states.keys() ]
-				.filter( clientId => this.seenStates.has( clientId ) )
 				.filter( clientId => {
 					// Exclude any users without `userInfo`.
 					// This can happen from the Yjs inspector, which joins the awareness
